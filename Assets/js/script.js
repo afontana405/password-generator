8948f0b9cfc230f8582d53bc2480616f0c6a1d87
afontana405 <alex13579@icloud.com>
2023-09-25 14:37:06 -0500

    finalized the code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,6 +18,7 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var choices = []
+  var genPassword = []
   var lowercaseConfirm = confirm("Would you like your password to contain lowercase letters?")
   var uppercaseConfirm = confirm("Would you like your password to contain uppercase letters?")
   var numbersConfirm = confirm("Would you like your password to contain numbers?")
@@ -27,48 +28,49 @@ function generatePassword() {
   if(lowercaseConfirm) {
     // if confirmed by user, adds lowercase letters to var choices
     choices = choices.concat(lowercase);
-  } else {
-    
-  }
-  
+  } 
+
   // user imput decides if uppercase letters will be used in password
   if(uppercaseConfirm) {
     // if confirmed by user, adds uppercase letters to var choices
     choices = choices.concat(uppercase);
-  } else {
-
-  }
+  } 
 
   // user imput decides if numbers will be used in password
   if(numbersConfirm) {
     // if confirmed by user, adds numbers to var choices
     choices = choices.concat(numbers);
-  } else {
-
   }
 
   // user imput decides if special characters will be used in password
   if(specialConfirm) {
     // if confirmed by user, adds special characters to var choices
     choices = choices.concat(special);
-  } else {
-
   }
 
+  // checks to make sure user imput into var is a number
+  if(isNaN(passwordLength)) {
+    alert("insert a number between 8-128");
+    return
+  }
   // checks to make sure password length is between 8-128 characters
   if(passwordLength < 8 || passwordLength > 128) {
     // if password is too long/short tells user password must be between 8-128 characters and restarts the function
     alert("password must be between 8-128 characters");
-    generatePassword();
-    } else {
+    return
+  } 
 
-    }
+  // makes sure at least one character is selected
+  if(choices.length === 0) {
+    alert("password must contain at least one character");
+    return
+  }
   
   // randomly picks characters to be used in user's password based off their preferences and desired length
   for(i = 0; i < passwordLength; i++) {
   var randomNum = Math.floor(Math.random() * choices.length);
-  var genPassword = "".concat(choices[randomNum]);
-  console.log(genPassword);
+  genPassword = genPassword.concat(choices[randomNum]);
   }
+  return genPassword.join("");
 }
 
